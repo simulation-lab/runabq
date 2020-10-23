87bbda3b09cff21c8aa22d75919705234044fff5
Mamoru Kinomura <kinomura.mamoru@gmail.com>
2020-10-24 00:19:26 +0900

    test_args.pyの作成

---
+++ tests/test_args.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+class TestArgs:
+
+    @pytest.mark.parametrize('version, expected', [
+        ('2020', 'abq2020'),
+    ])
+    def test_get_solver_version(self, mocker, version, expected):
+        from runabq.args import get_solver_version
+        from runabq import args
+        insmock = mocker.Mock()
+        insmock.which.return_value = version
+        mocker.patch.object(args, 'which',
+                            mocker.Mock(return_value=insmock))
+        assert get_solver_version(version) == expected
+
+    @ pytest.mark.parametrize('keyword, expected', [
+        (['user=job1.f', 'cpus=3'], 'user=job1.f cpus=3'),
+    ])
+    def test_get_arg_terms(self, keyword, expected):
+        from runabq.args import get_arg_terms
+        assert get_arg_terms(keyword) == expected
