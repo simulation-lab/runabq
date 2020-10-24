41a53fd594a52136172a70c11b6a69832caeb5e5
Mamoru Kinomura <kinomura.mamoru@gmail.com>
2020-10-24 14:49:55 +0900

    テストコードの編集

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,28 @@
 import pytest
 
 
-class TestRunabq:
-    @pytest.mark.parametrize('keyword, version', [('user=job1.f', '2020')])
-    def test_runabq(self, keyword, version):
+class TestCore:
+
+    @pytest.fixture
+    def scenario_data(self):
+        target_files = {
+            'target_input_files': (f for f in ['job1.inp', 'job2.inp']),
+            'total_job_num': 2
+        }
+        return {
+            'solver_version': 'abq2020',
+            'arg_terms': 'user=sub.f',
+            'files': target_files,
+            'expected': None
+        }
+
+    def test_runabq_stdout(self, capfd, scenario_data):
         from runabq.core import runabq
-        pass
+        from runabq import core
+
+        keyword = scenario_data['arg_terms']
+        version = scenario_data['solver_version']
+        runabq(keyword, version)
+        out, err = capfd.readouterr()
+        assert out.strip() == 'abq-command not found.'
+        assert err is ''
