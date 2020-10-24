4baeb7f0539a3ae4b4e0c0796ab079ac1aee059b
Mamoru Kinomura <kinomura.mamoru@gmail.com>
2020-10-25 03:01:34 +0900

    複数のパラメータを追加できるコードに変更

--- tests/test_command.py
+++ tests/test_command.py
@@ -3,20 +3,19 @@ import pytest
 
 class TestCommand:
 
-    @pytest.fixture
-    def command_args(self):
-        return {
-            'solver_version': '2020',
-            'arg_terms': 'cpus=3',
-            'target_input_files': ['job1.inp', 'job3.inp'],
-            'total_job_num': 3,
-            'debug': True,
-        }
-
     @pytest.mark.parametrize(
-        'expected', ("['2020', 'interactive', '-j', 'job3', 'cpus=3']",)
-    )
-    def test_run_command(self, capfd, mocker, command_args, expected):
+        'command_args, expected', [
+            ({
+                'solver_version': '2020',
+                'arg_terms': 'cpus=3',
+                'target_input_files': ['job1.inp', 'job3.inp'],
+                'total_job_num': 3,
+                'debug': True,
+            },
+                "['2020', 'interactive', '-j', 'job3', 'cpus=3']",
+            ),
+        ])
+    def test_run_command(self, capfd, command_args, expected):
         from runabq.command import run_command
         run_command(
             solver_version=command_args['solver_version'],
