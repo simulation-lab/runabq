7c1400a8203f0bd76b34fb577585f306f92949a8
Mamoru Kinomura <kinomura.mamoru@gmail.com>
2020-10-25 02:54:52 +0900

    test_command.pyへのテスコトードの追加

---
+++ tests/test_command.py
@@ -0,0 +1,32 @@
+import pytest
+
+
+class TestCommand:
+
+    @pytest.fixture
+    def command_args(self):
+        return {
+            'solver_version': '2020',
+            'arg_terms': 'cpus=3',
+            'target_input_files': ['job1.inp', 'job3.inp'],
+            'total_job_num': 3,
+            'debug': True,
+        }
+
+    @pytest.mark.parametrize(
+        'expected', ("['2020', 'interactive', '-j', 'job3', 'cpus=3']",)
+    )
+    def test_run_command(self, capfd, mocker, command_args, expected):
+        from runabq.command import run_command
+        run_command(
+            solver_version=command_args['solver_version'],
+            arg_terms=command_args['arg_terms'],
+            target_input_files=command_args['target_input_files'],
+            total_job_num=command_args['total_job_num'],
+            debug=command_args['debug']
+        )
+        out, err = capfd.readouterr()
+        out_list = out.split('\n')
+        out_list = [a for a in out_list if a.strip()]
+        assert out_list[-1] == expected
+        assert err == ''
