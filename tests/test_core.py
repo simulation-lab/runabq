70739852fd5c07da8eccc33667f54043300e1ba1
Mamoru Kinomura <kinomura.mamoru@gmail.com>
2020-11-08 11:16:03 +0900

    テストの追加

--- tests/test_core.py
+++ tests/test_core.py
@@ -26,3 +26,42 @@ class TestCore:
         out, err = capfd.readouterr()
         assert out.strip() == 'abaqus command not found.'
         assert err == ''
+
+    def test_run_function(self, mocker):
+        from click.testing import CliRunner
+        from runabq.core import run
+        from runabq import core
+        mocker.patch.object(core, 'runabq', return_value=None)
+        runner = CliRunner()
+        result = runner.invoke(run)
+        assert result.exit_code == 0
+        assert result.output == 'finished.\n'
+
+    @pytest.fixture
+    def commandline_option(self):
+        target_files = ['test1.inp', 'test2.inp', 'test3.inp']
+        return {
+            'keyword': ['cpus=5'],
+            'version': 'latest',
+            'solver_version': 'abaqus',
+            'arg_terms': 'cpus=5 user=test.f90',
+            'get_target_files': {
+                'target_input_files': (f for f in target_files),
+                'total_job_num': len(target_files)
+            },
+            'debug': True
+        }
+
+    def test_runabq(self, mocker, commandline_option):
+        from runabq import core
+        co = commandline_option
+        mocker.patch.object(core, 'get_solver_version',
+                            return_value=co['solver_version'])
+        mocker.patch.object(core, 'get_arg_terms',
+                            return_value=co['arg_terms'])
+        mocker.patch.object(core, 'get_target_files',
+                            return_value=co['get_target_files'])
+        mocker.patch.object(core, 'run_command', return_value=None)
+        result = core.runabq(
+            keyword=co['keyword'], version=co['version'], debug=co['debug'])
+        assert result == None
