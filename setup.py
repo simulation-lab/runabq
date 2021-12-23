74d6b28492ca89cf400f376f1fb21515521f6e6d
Mamoru Kinomura <kinomura.mamoru@gmail.com>
2021-12-23 12:08:53 +0900

    バージョンの修正

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ of FEA software Abaqus in succession."""
 
 setup(
     name='runabq',
-    version='1.0.1',
+    version='1.0.2',
 
     author='kino',
     author_email='simulation.space.labs@gmail.com',
